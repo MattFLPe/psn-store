0becf194050ed8a8796feedec4d3f8ff9dc73035
MattFLPe <brasil.mateus748@gmail.com>
2023-10-08 00:44:38 -0300

    Built HTML structure for the components and styled it with CSS

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
   styleUrls: ['./card-pricing.component.css']
 })
-export class CardPricingComponent {
+export class CardPricingComponent implements OnInit {
+
+    @Input()
+    consolePlatform:string = "Digital PS5"
+
+    @Input()
+    gamePrice:string = "R$ 149,99"
+
+    constructor(){ }
+
+  ngOnInit(): void {
+    ;
+  }
 
 }
