c00493e996ce43371fa36c89fa4c9f4e52cb23a0
MattFLPe <brasil.mateus748@gmail.com>
2023-10-08 12:55:25 -0300

    Added dynamic card-label and card-pricing using TypeScript

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -7,11 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardPricingComponent implements OnInit {
 
-    @Input()
-    consolePlatform:string = "Digital PS5"
-
-    @Input()
-    gamePrice:string = "R$ 149,99"
+  @Input()
+  consolePlatform:string = ""
+  @Input()
+  gamePrice:string = ""
 
     constructor(){ }
 
